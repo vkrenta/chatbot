54ee8ff9ac8e345ce4926df14c8eea9385fd6d46
vkrenta <0vkrenta@gmail.com>
2019-12-28 13:08:07 +0200

    referral system - done: check if link activated twice

--- features/referral.js
+++ features/referral.js
@@ -3,6 +3,8 @@ const refs = require('../models/refs')
 module.exports = (controller) => {
   controller.hears('Invite a friend', 'message,direct_message,facebook_postback', async (bot, message) => {
     const reflink = `m.me/${message.recipient.id}?ref=${message.user}`
+    const menu = require('../attachments/main_menu.json')
+    menu.text = 'Send link to 3 friends and get a gift (O.O)'
     console.log(reflink)
 
     const query = {
@@ -11,7 +13,8 @@ module.exports = (controller) => {
     }
 
     await refs.findOrCreate(query)
-      .then((result) => bot.reply(message, `Your referral link is: ${reflink}`))
+      .then(() => bot.reply(message, `Your referral link is: ${reflink}`))
+      .then(() => bot.reply(message, menu))
       .catch(reason => console.log(reason))
   })
 }
