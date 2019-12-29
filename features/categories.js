64e170dd2236d8fd9397dd37867da5a795d314d0
vkrenta <0vkrenta@gmail.com>
2019-12-29 17:15:33 +0200

    added shop button functionality

--- features/categories.js
+++ features/categories.js
@@ -1,9 +1,10 @@
 const bby = require('../modules/bbyapi')
 
 module.exports = (controller) => {
-  controller.on('facebook_postback', async (bot, message) => {
+  controller.on('message,direct_message,facebook_postback', async (bot, message) => {
     // Catalogue of items
-    if (message.text === 'GATALOGUE_PAYLOAD') {
+    if (message.text === 'GATALOGUE_PAYLOAD' ||
+      (message.quick_reply && message.quick_reply.payload === 'SHOP_PAYLOAD')) {
       const catalogue = {
         messaging_type: 'RESPONSE',
         text: 'Select category',
