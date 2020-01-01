5646efe9e9e724940f8bcf5cb9cac5b02ccdf74e
vkrenta <0vkrenta@gmail.com>
2020-01-01 22:01:42 +0200

    added rate buttons

--- features/test_dialog.js
+++ features/test_dialog.js
@@ -29,14 +29,31 @@ module.exports = (controller) => {
     quick_replies
   }
 
+  const backToMenu = {
+    text: 'Our courier will contact you within 2 hours',
+    quick_replies: [
+      {
+        content_type: 'text',
+        title: 'Back to main menu',
+        payload: 'MAIN_MENU_PAYLOAD'
+      }
+    ]
+  }
+
+  const rate = {
+    text: 'Pls, tell me did you like the product. ' +
+    'How do you estimate recommend our proguct to your friends?',
+    quick_replies: []
+  }
+  for (let index = 1; index <= 10; index++) {
+    rate.quick_replies.push({
+      content_type: 'text',
+      title: `${index}`,
+      payload: 'RATE_PAYLOAD'
+    })
+  }
+
   convo.say('Hmm, nice choise')
-  // convo.ask('Please, share your phone, format 0DDDDDDDDD ' +
-  // '(example 0123456789)', async (response, convo, bot) => {
-  //   if (!/0\d{9}\b/.test(response)) {
-  //     await bot.say('Incorrect phone number. Try again')
-  //     await convo.repeat()
-  //   }
-  // }, 'phone')
   convo.addAction('getting_phone')
   convo.addQuestion('Please, share your phone, format 0DDDDDDDDD ' +
   '(example 0123456789)', async (response, convo, bot) => {
@@ -61,14 +78,27 @@ module.exports = (controller) => {
   }, 'address', 'getting_address')
 
   convo.addAction('confirm_address')
-  convo.addQuestion(answerAddress, async (response, convo, bot) => {
-    if (response.toLowerCase() === 'yes') return await bot.say('Our courier will contact you within 2 hours')
-    if (response.toLowerCase() === 'no') return await convo.gotoThread('getting_address')
-    await bot.say('Incorrect answer. Try again')
-    return await convo.repeat()
+  convo.addQuestion(answerAddress, (response, convo, bot) => {
+    if (response.toLowerCase() === 'yes') {
+      console.log('before timeout ' + Date.now())
+      bot.say(backToMenu)
+      return new Promise(resolve => setTimeout((resolve), 1000))
+        .then(() => {
+          console.log('after ' + Date.now())
+          bot.say(rate)
+        })
+    }
+    if (response.toLowerCase() === 'no') return convo.gotoThread('getting_address')
+    bot.say('Incorrect answer. Try again')
+      .then(() => convo.repeat())
   }, '_adconfirmed', 'confirm_address')
 
+  convo.after(async (results, bot) => {
+
+  })
+
   controller.addDialog(convo)
+
   controller.hears('test', 'message,direct_message,facebook_postback', async (bot, message) => {
     console.log('dwdddddddddddddddddddddddddddddd')
     await bot.beginDialog(DIALOG_ID)
