8969a07ed9271bd9c30488432b3d8d649ccde5e9
vkrenta <0vkrenta@gmail.com>
2020-01-02 15:54:08 +0200

    removed async await

--- features/referral.js
+++ features/referral.js
@@ -1,18 +1,17 @@
 const refs = require('../models/refs')
 
 module.exports = (controller) => {
-  controller.hears('Invite a friend', 'message,direct_message,facebook_postback', async (bot, message) => {
+  controller.hears('Invite a friend', 'message,direct_message,facebook_postback', (bot, message) => {
     const reflink = `m.me/${message.recipient.id}?ref=${message.user}`
     const menu = require('../attachments/main_menu.json')
     menu.text = 'Send link to 3 friends and get a gift (O.O)'
-    console.log(reflink)
 
     const query = {
       creatorId: message.user,
       reflink: reflink
     }
 
-    await refs.findOrCreate(query)
+    return refs.findOrCreate(query)
       .then(() => bot.reply(message, `Your referral link is: ${reflink}`))
       .then(() => bot.reply(message, menu))
       .catch(reason => console.log(reason))
