59ffe944d4e607d5f2b3f68eabbfc296b9dc8f45
vkrenta <0vkrenta@gmail.com>
2020-01-05 21:46:56 +0200

    fixes in bby and bby tests

--- tests/bbytest.js
+++ tests/bbytest.js
@@ -1,49 +1,70 @@
 /* eslint-disable no-unused-expressions */
 const { describe, it } = require('mocha')
+const assert = require('assert')
 const { expect } = require('chai')
 const bby = require('../modules/bbyapi')
 
 describe('Testing BestBuy API: ', () => {
-  it('Get total products by category', async () => {
-    await bby.getTotalProducts('abcat0102000')
+  it('Get total products by category', () => {
+    return bby.getTotalProducts('abcat0102000')
       .then((data) => {
         console.log(`Total products : ${data}`)
         expect(data !== null).to.be.true
       })
+      .catch(err => {
+        console.error(err.status, err.statusText)
+        assert.fail(err)
+      })
   })
 
-  it('Get total categories ', async () => {
-    await bby.getTotalCategories()
+  it('Get total categories ', () => {
+    return bby.getTotalCategories()
       .then((data) => {
         console.log(`Total categories : ${data}`)
         expect(data !== null).to.be.true
       })
+      .catch(err => {
+        console.error(err.status, err.statusText)
+        assert.fail(err)
+      })
   })
 
-  it('Get list of all categories: ', async () => {
-    await bby.getListOfCategories(1, 10)
+  it('Get list of all categories: ', () => {
+    return bby.getListOfCategories(1, 10)
       .then((data) => {
         console.log('Categories: ')
         console.log(data)
         expect(data !== null).to.be.true
       })
+      .catch(err => {
+        console.error(err.status, err.statusText)
+        assert.fail(err)
+      })
   })
 
-  it('Get products by category id: ', async () => {
-    await bby.getProductsByCategory('abcat0102000', 1, 4)
+  it('Get products by category id: ', () => {
+    return bby.getProductsByCategory('abcat0102000', 1, 4)
       .then(data => {
         console.log('Products: ')
         console.log(data)
         expect(data !== null).to.be.true
       })
+      .catch(err => {
+        console.error(err.status, err.statusText)
+        assert.fail(err)
+      })
   })
 
-  it('Get product by sku: ', async () => {
-    await bby.getProductBySku(4743301)
+  it('Get product by sku: ', () => {
+    return bby.getProductBySku(4743301)
       .then(data => {
         console.log('Product with sku 4743301: ')
         console.log(data)
         expect(data !== null).to.be.true
+      })
+      .catch(err => {
+        console.error(err.status, err.statusText)
+        assert.fail(err)
       })
   })
 })
